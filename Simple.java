08e8da7a65d1c94020a29f8cb0f220d72f5d592c
Sufian <sa5528009@gmail.com>
2022-10-06 23:39:43 +0530

    Modified Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,11 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java\n");  
+     System.out.println("Hello Sufian\n");
+     int a=10,b=20;
+     int sum=a+b;
+     System.out.println("a is:"+a);  
+     System.out.println("b is:"+b);
+     System.out.println("sum of a and b is:"+sum);
     }  
 }  
