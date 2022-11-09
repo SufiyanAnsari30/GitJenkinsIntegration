db460ab8df5341ce2723af66047ee06e74525236
Sufian <sa5528009@gmail.com>
2022-11-09 19:59:33 +0530

    Added Simple2.java file

---
+++ Simple2.java
@@ -0,0 +1,11 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java\n");  
+     System.out.println("Hello Sufian Ansari\n");
+     int a=100,b=200;
+     int sum=a+b;
+     System.out.println("a is:"+a);  
+     System.out.println("b is:"+b);
+     System.out.println("sum of a and b is:"+sum);
+    }  
+}  
